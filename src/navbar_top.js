922e8e7f8363fef1c97e7fc5ec4837e7bc8e93c9
João Pedro Barros <jpedro@server01.foursys.local>
2022-07-23 09:40:18 -0300

    Adiconando login page

---
+++ src/navbar_top.js
@@ -0,0 +1,13 @@
+import styles from "../styles/components/navbar_top.module.css"
+
+function Navbar_top(){
+    return(
+        <div className={styles.navbar_top}>
+            <div className={styles.logo}>
+                <h1>ok</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Navbar_top
